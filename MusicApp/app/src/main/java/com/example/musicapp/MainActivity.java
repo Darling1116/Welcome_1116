7e38063263679940662d6479714d1446bb4d31e0
Darling1116 <1154883599@qq.com>
2021-11-20 23:44:52 +0800

    提交代码

--- MusicApp/app/src/main/java/com/example/musicapp/MainActivity.java
+++ MusicApp/app/src/main/java/com/example/musicapp/MainActivity.java
@@ -1,27 +1,100 @@
 package com.example.musicapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
-    private Button btnEnter;
+public class MainActivity extends AppCompatActivity  implements View.OnClickListener{
+    ImageView nextIv, playIv, lastIv;
+    TextView singerTv, songTv;
+    RecyclerView musicRv;
+    //数据源
+    List<LocalMusicBean> mDatas;
+    private LocalMusicAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+        mDatas = new ArrayList<>();
+        //设置配置对象
+        adapter = new LocalMusicAdapter(this, mDatas);
+        musicRv.setAdapter(adapter);
+        //设置布局管理器
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this, RecyclerView.VERTICAL, false);
+        musicRv.setLayoutManager(layoutManager);
 
-        btnEnter = findViewById(R.id.btn_enter);
-        btnEnter.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
-                Intent in = new Intent(MainActivity.this, MusicActivity.class);
-                startActivity(in);
-            }
-        });
+        //加载本地数据源
+        loadLocalMusicData();
     }
+
+    private void loadLocalMusicData() {
+        //加载本地存储当中的音乐mp3文件到集合当中
+        //1.获取ContentResolver对象
+        ContentResolver resolver = getContentResolver();
+        //2.
+        Uri uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+        //3.
+        Cursor cursor = resolver.query(uri, null, null, null, null);
+        //4.
+        int id = 0;
+        while (cursor.moveToNext()) {
+            String song = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.TITLE));
+            String singer = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.ARTIST));
+            String album = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.ALBUM));
+            id++;
+            String sid = String.valueOf(id);
+            String path = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.DATA));
+            long duration = cursor.getLong(cursor.getColumnIndex(MediaStore.Audio.Media.DURATION));
+            SimpleDateFormat sdf = new SimpleDateFormat("mm:ss");
+            String time = sdf.format(new Date(duration));
+            //将一行当中的数据封装到对象当中
+            LocalMusicBean bean = new LocalMusicBean(sid, song, singer, album, time, path);
+            mDatas.add(bean);
+        }
+        //数据源变化，提示适配器更新
+        adapter.notifyDataSetChanged();
+    }
+
+    private void initView() {
+        nextIv = findViewById(R.id.local_music_botton_iv_next);
+        playIv = findViewById(R.id.local_music_botton_iv_play);
+        lastIv = findViewById(R.id.local_music_botton_iv_last);
+        singerTv = findViewById(R.id.local_music_botton_iv_singer);
+        songTv = findViewById(R.id.local_music_botton_iv_song);
+        musicRv = findViewById(R.id.local_music_rv);
+        nextIv.setOnClickListener(this);
+        lastIv.setOnClickListener(this);
+        playIv.setOnClickListener(this);
+    }
+
+
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.local_music_botton_iv_last:
+                break;
+            case R.id.local_music_botton_iv_next:
+                break;
+            case R.id.local_music_botton_iv_play:
+                break;
+        }
+    }
+
 }

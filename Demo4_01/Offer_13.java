8f7b9c769a11bb469f37c9384da31d6a1b6604ce
Darling1116 <1154883599@qq.com>
2022-04-03 00:30:15 +0800

    提交代码

--- Demo4_01/Offer_13.java
+++ Demo4_01/Offer_13.java
@@ -24,8 +24,24 @@ public class Offer_13 {
 		return sum;
 	}
 
+	public static int DFS(boolean[][] flag, int i, int j, int k) {
+		if (i < 0 || i >= flag.length || j < 0 || j >= flag[0].length
+				|| getSum(i) + getSum(j) > k || flag[i][j])
+			return 0;
+
+		flag[i][j] = true;
+		return DFS(flag, i, j + 1, k) + DFS(flag, i, j - 1, k)
+				+ DFS(flag, i - 1, j, k) + DFS(flag, i + 1, j, k) + 1;
+	}
+
+	public static int movingCount(int m, int n, int k) {
+		boolean[][] flag = new boolean[m][n];
+		return DFS(flag, 0, 0, k);
+	}
+
 	public static void main(String[] args) {
 		// System.out.println(getSum(26));
-		System.out.println(movingCount_1(3, 2, 1));
+		// System.out.println(movingCount_1(3, 2, 1));
+		System.out.println(movingCount(3, 1, 0));
 	}
 }
